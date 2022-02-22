2785739997eaf8d261a837d0cc36402a8109b592
noleykuz <noleykuzfranklin@gmail.com>
2022-02-22 14:54:59 +0100

    day1

--- day1/script/day1.js
+++ day1/script/day1.js
@@ -1,5 +1,12 @@
 function day1() {
 	var nanme = (document.getElementsByTagName('body')[0].innerHTML =
 		'Testing the mic2');
+		// Things to cover for today
+		/** comments , syntax and variables */
+		// variables  let,const,var
+		var mmicheal=2;
+		var mmicheal=2;
+		let divine=2;
+		let divine=2;
 }
 day1();
